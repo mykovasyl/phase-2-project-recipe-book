3299f29ef5540c9671f52525202f9e2836505e7d
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-05-09 09:55:10 -0400

    update like button state for multiple component use

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Recipe({
   recipe,
@@ -33,30 +33,6 @@ function Recipe({
     }
   }
 
-  // let likeDeleteButton = liked ? (
-  //   <button type="button" onClick={handleClick}>
-  //     Delete it!
-  //   </button>
-  // ) : (
-  //   <button type="button" onClick={handleClick}>
-  //     Like it!
-  //   </button>
-  // );
-
-  // if (isRecipeBook || likedRecipe) {
-  //   likeDeleteButton = (
-  //     <button type="button" onClick={handleClick}>
-  //       Delete it!
-  //     </button>
-  //   );
-  // } else {
-  //   likeDeleteButton = (
-  //     <button type="button" onClick={handleClick}>
-  //       Like it!
-  //     </button>
-  //   );
-  // }
-
   return (
     <div>
       <h1>{title}</h1>
@@ -74,7 +50,7 @@ function Recipe({
       )}
 
       <h3>Ready in {readyIn} minutes</h3>
-      <img src={image} alt="image of food"></img>
+      <img src={image} alt="food"></img>
       <h3>Summary:</h3>
       <div dangerouslySetInnerHTML={{ __html: summary }} />
       <h3>Ingredients:</h3>
